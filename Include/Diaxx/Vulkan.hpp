d79b0e79a6c6e6d80bbc29d22f94a88f5d7279fb
MrKolzy <kolzykun@gmail.com>
2025-10-04 10:35:34 +0200

    Instance

--- Include/Diaxx/Vulkan.hpp
+++ Include/Diaxx/Vulkan.hpp
@@ -3,7 +3,9 @@
 #define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
 
 #include <GLFW/glfw3.h>
-#include <vulkan/vulkan.hpp>
+#include <vulkan/vulkan_raii.hpp>
+
+#include <cstdint> // std::uint32_t
 
 namespace Diaxx
 {
@@ -16,11 +18,18 @@ namespace Diaxx
 		void run();
 
 	private:
-		void initializeWindow();
-		void initializeVulkan();
+		void initializeWindow(); // 1.0
+
+		void initializeVulkan(); // 2.0
+
+		void createInstance();   // 2.1
+		void checkExtensions(std::uint32_t glfwExtensionCount, const auto& glfwExtensions);
+
 		void mainLoop();
 		void cleanup();
 
-		GLFWwindow* m_window {};
+		GLFWwindow*        m_window   {};
+		vk::raii::Context  m_context  {};
+		vk::raii::Instance m_instance { nullptr };
 	};
 }
--- Source/Vulkan.cpp
+++ Source/Vulkan.cpp
@@ -2,6 +2,13 @@
 
 #include "Diaxx/Constants.hpp"
 
+#include <algorithm>   // std::ranges
+#include <cstring>     // std::strcmp
+#include <print>       // std::print
+#include <stdexcept>   // std::runtime_error
+#include <string>      // std::string
+#include <string_view> // std::string_view
+
 namespace Diaxx
 {
 	void Vulkan::run()
@@ -24,8 +31,62 @@ namespace Diaxx
 			"Diaxx", nullptr, nullptr);
 	}
 
-	void Vulkan::initializeVulkan()
+	void Vulkan::initializeVulkan() // 2.0
 	{
+		createInstance(); // 2.1
+	}
+
+	// The instance is the connection between your application and the Vulkan library
+	void Vulkan::createInstance()
+	{
+		// Optional structure with information about our application
+		constexpr vk::ApplicationInfo appInfo {
+			.pApplicationName   = "Diaxx",
+			.applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+			.pEngineName        = "Diaxx Engine",
+			.engineVersion      = VK_MAKE_VERSION(1, 0, 0),
+			.apiVersion         = vk::ApiVersion14
+		};
+
+		// Get the necessary extensions for the GLFW library
+		std::uint32_t glfwExtensionCount {};
+		const auto glfwExtensions { glfwGetRequiredInstanceExtensions(&glfwExtensionCount) };
+
+		checkExtensions(glfwExtensionCount, glfwExtensions);
+
+		// Structure that tells Vulkan which global extensions and validation layers to use
+		const vk::InstanceCreateInfo createInfo {
+			.pApplicationInfo        = &appInfo,
+			.enabledExtensionCount   = glfwExtensionCount,
+			.ppEnabledExtensionNames = glfwExtensions
+		};
+
+		// Issue the call to create an instance
+		m_instance = vk::raii::Instance(m_context, createInfo);
+	}
+
+	// Check if the extensions required for the GLFW library are compatible with Vulkan
+	void Vulkan::checkExtensions(std::uint32_t glfwExtensionCount, const auto& glfwExtensions)
+	{
+		const auto extensionProperties { m_context.enumerateInstanceExtensionProperties() };
+		for (std::uint32_t i {}; i < glfwExtensionCount; ++i)
+		{
+			if (std::ranges::none_of(extensionProperties,
+				[glfwExtension = glfwExtensions[i]](const auto& extensionProperty)
+				{ return std::strcmp(extensionProperty.extensionName, glfwExtension) == 0; }))
+			{
+				throw std::runtime_error("The " + std::string(glfwExtensions[i]) +
+					" extension required by GLFW is not supported.\n");
+			}
+		}
+
+		std::println("[Debug]: List of supported Vulkan instance extensions:");
+		for (const auto& extension : extensionProperties)
+			std::println("\t- {}", std::string_view(extension.extensionName));
+
+		std::println("\n[Debug]: List of extensions required by the GLFW library:");
+		for (std::uint32_t i {}; i < glfwExtensionCount; ++i)
+			std::println("\t- {}", std::string_view(glfwExtensions[i]));
 	}
 
 	void Vulkan::mainLoop()
