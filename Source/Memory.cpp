d85dde99be31edead5c1c623e8389c4e4a8f229d
MrKolzy <kolzykun@gmail.com>
2026-03-09 13:01:05 +0100

    Small change

--- Source/Memory.cpp
+++ Source/Memory.cpp
@@ -7,7 +7,7 @@ namespace Diaxx
 	Memory::Memory(std::uint32_t processIdentifier)
 	{
 		constexpr DWORD desiredAccess { PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION };
-		m_process.reset(OpenProcess(desiredAccess, 0, static_cast<DWORD>(processIdentifier)));
+		m_process.reset(OpenProcess(desiredAccess, FALSE, static_cast<DWORD>(processIdentifier)));
 		if (!m_process)
 			throw std::runtime_error("The process could not be opened");
 	}
